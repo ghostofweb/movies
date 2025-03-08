8718ea1857665165a4ba315234e06321b266c560
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-08 23:15:31 +0530

    Add new tabs for Search, Profile, and Saved; update index screen with a link to movie details

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,9 @@ export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="search" options={{title:"Search"}}/>
+      <Tabs.Screen name="profile" options={{title:"Profile"}}/>
+      <Tabs.Screen name="saved" options={{title:"Saved"}}/>
     </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-light-100 font-bold">Welcome</Text>
+      <Link href={"/movies/[id]"}>HEllo world</Link>
     </View>
   );
 }
