31772a80e683713c21801bf96e116db5f72b6f70
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-09 11:37:45 +0530

    Add tab bar icon for Home screen using ImageBackground component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,16 @@
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+import { ImageBackground } from "react-native";
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="search" options={{title:"Search"}}/>
+      <Tabs.Screen name="index" options={{ title: "Home",tabBarIcon:({focused})=>(
+        <>
+        <ImageBackground source={images.highlight}/>
+        </>
+      ) }} />
+      <Tabs.Screen name="search" options={{title:"Search",}}/>
       <Tabs.Screen name="profile" options={{title:"Profile"}}/>
       <Tabs.Screen name="saved" options={{title:"Saved"}}/>
     </Tabs>
