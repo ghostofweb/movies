fa8590f16a02d727f497ab93dd3f92822e114cda
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-10 20:31:07 +0530

    Refactor tab layout to include background image and improve styling; update index screen with background and logo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,21 @@
 import { Tabs } from "expo-router";
-import { ImageBackground, Image, Text, View } from "react-native";
+import { ImageBackground, Image, Text, View, StyleSheet } from "react-native";
 
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
 function TabIcon({ focused, icon, title }: any) {
-  if (focused) {
-    return (
-      <ImageBackground
-        source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[114px] min-h-20 mt-4 justify-center items-center rounded-full overflow-hidden mr-2"
-      >
-        <Image source={icon} tintColor="#151312" className="size-5" />
-        <Text className="text-secondary text-base font-semibold ml-2">
-          {title}
-        </Text>
-      </ImageBackground>
-    );
-  }
-
-  return (
+  return focused ? (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[114px] min-h-20 mt-4 justify-center items-center rounded-full overflow-hidden mr-2"
+    >
+      <Image source={icon} tintColor="#151312" className="size-5" />
+      <Text className="text-secondary text-base font-semibold ml-2">
+        {title}
+      </Text>
+    </ImageBackground>
+  ) : (
     <View className="size-full justify-center items-center mt-4 rounded-full">
       <Image source={icon} tintColor="#A8B5DB" className="size-5" />
     </View>
@@ -28,71 +24,80 @@ function TabIcon({ focused, icon, title }: any) {
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarItemStyle: {
-          width: "100%",
-          height: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        },
-        tabBarStyle: {
-          backgroundColor: "#0F0D23",
-          borderRadius: 25,
-          marginHorizontal: 20,
-          marginBottom: 36,
-          height: 52,
-          position: "absolute",
-          overflow: "hidden",
-          borderWidth: 1,
-          borderColor: "#0F0D23",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "index",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.home} title="Home" />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.search} title="Search" />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="saved"
-        options={{
-          title: "Save",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.save} title="Save" />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.person} title="Profile" />
-          ),
-        }}
-      />
-    </Tabs>
+    <ImageBackground source={images.bg} style={styles.background}>
+      <View style={styles.container}>
+        <Tabs
+          screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            tabBarStyle: {
+              backgroundColor: "#0F0D23",
+              borderRadius: 25,
+              marginHorizontal: 20,
+              marginBottom: 36,
+              height: 52,
+              position: "absolute",
+              overflow: "hidden",
+              borderWidth: 1,
+              borderColor: "#0F0D23",
+            },
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.home} title="Home" />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="search"
+            options={{
+              title: "Search",
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.search} title="Search" />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="saved"
+            options={{
+              title: "Save",
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.save} title="Save" />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="profile"
+            options={{
+              title: "Profile",
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.person} title="Profile" />
+              ),
+            }}
+          />
+        </Tabs>
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  container: {
+    flex: 1,
+  },
+});
