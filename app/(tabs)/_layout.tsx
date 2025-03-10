e79b8c5771e1a54383df4ac6b66361ccb0b1592d
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-10 10:26:35 +0530

    Add custom tab icon for Home screen with ImageBackground and label

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,25 @@
+import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
-import { ImageBackground } from "react-native";
+import { Image, ImageBackground, Text } from "react-native";
 
+const TabIcon = ()=>{
+  return(
+    <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center overflow-hidden">
+    <Image source={icons.home}/>
+     <Text className="text-secondary text-base font-semibold mg-2">Home</Text>
+  </ImageBackground>
+  )
+}
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: "Home",tabBarIcon:({focused})=>(
         <>
-        <ImageBackground source={images.highlight}/>
-        </>
+        <TabIcon/>
+       </>
       ) }} />
-      <Tabs.Screen name="search" options={{title:"Search",}}/>
+      <Tabs.Screen name="search" options={{title:"Search", }}/>
       <Tabs.Screen name="profile" options={{title:"Profile"}}/>
       <Tabs.Screen name="saved" options={{title:"Saved"}}/>
     </Tabs>
